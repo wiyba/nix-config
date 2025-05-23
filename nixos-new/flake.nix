8af5b0633decced4102d0a87a4374ee7ec9c99b7
wiyba <account@wiyba.org>
2025-05-23 22:01:01 +0300

    update

---
+++ nixos-new/flake.nix
@@ -0,0 +1,17 @@
+{
+	description = "nixos & home-manager configuration files by wiyba";
+
+	nixConfig = {
+		extra-substitures = [
+			"https://cache.nixos.org"
+			"https://cache.garnix.io"
+		];
+		extra-trusted-public-keys = [
+			
+		];
+	};
+
+	inputs = {
+		
+	};	
+}
