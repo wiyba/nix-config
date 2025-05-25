83234e0004eea8a5b16999a53cfbd57acf08730f
Dmitry Shmakov <81859776+wiyba@users.noreply.github.com>
2025-05-25 15:35:28 +0300

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,3 +1,17 @@
 {
+	description = "nixos & home-manager configuration files by wiyba";
 
+	nixConfig = {
+	};
+
+	inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+
+    flake-utils.url = "github:numtide/flake-utils";
+    nix-darwin.url = "github:LnL7/nix-darwin";
+
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+  };
 }
