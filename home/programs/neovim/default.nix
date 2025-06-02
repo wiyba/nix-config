598efd9f91c37974e173c5c4a370475042f6d0dc
Dmitry Shmakov <81859776+wiyba@users.noreply.github.com>
2025-06-02 13:08:21 +0300

    Update default.nix

--- home/programs/neovim/default.nix
+++ home/programs/neovim/default.nix
@@ -4,6 +4,35 @@
   programs.neovim = {
     enable = true;
     defaultEditor = true;
-    extraPackages = with pkgs; [ tree-sitter ];
+    plugins = with pkgs.vimPlugins; [
+	    nvim-autopairs
+	    vim-sleuth
+	    nvim-cmp
+	    cmp-buffer
+	    lualine-nvim
+	    nvim-web-devicons
+	  ];
+	
+	  extraConfig = ''
+	    lua << EOF
+	      require('nvim-autopairs').setup()
+	      require('lualine').setup {
+	        options = {
+	          icons_enabled = true,
+	          theme = 'auto'
+	        }
+	      }
+	
+	      local cmp = require'cmp'
+	      cmp.setup {
+	        sources = {
+	          { name = 'buffer' }
+	        },
+	        mapping = cmp.mapping.preset.insert({
+	          ['<Tab>'] = cmp.mapping.confirm({ select = true })
+	        })
+	      }
+	    EOF
+	  '';
   };
 }
