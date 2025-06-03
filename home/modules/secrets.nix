54e2e718228b433a631524c457c97a1da7ce5f97
wiyba <account@wiyba.org>
2025-06-03 16:48:19 +0300

    update

--- home/modules/secrets.nix
+++ home/modules/secrets.nix
@@ -1,5 +1,8 @@
 {
-  vless_ip = { file = ../secrets/ip.age; };
-  vless_uuid = { file = ../secrets/uuid.age; };
-  vless_sid = { file = ../secrets/sid.age; };
+  age.identityPaths = [ "/home/wiyba/.config/age/identity.txt" ];
+  age.secrets = {
+    vless_ip = { file = ../secrets/ip.age; };
+    vless_uuid = { file = ../secrets/uuid.age; };
+    vless_sid = { file = ../secrets/sid.age; };
+  };
 }
