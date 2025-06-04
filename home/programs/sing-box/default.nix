cba2cbcf4226d134e48d694142e9258600e802f9
Dmitry Shmakov <81859776+wiyba@users.noreply.github.com>
2025-06-04 15:32:04 +0300

    Update default.nix

--- home/programs/sing-box/default.nix
+++ home/programs/sing-box/default.nix
@@ -4,56 +4,56 @@
   services.sing-box = {
     enable = true;
 
-    settings = let
-      ip = lib.strings.trim (builtins.readFile config.sops.secrets.ip.path);
-      uuid = lib.strings.trim (builtins.readFile config.sops.secrets.uuid.path);
-      sid = lib.strings.trim (builtins.readFile config.sops.secrets.sid.path);
-      
-    in {
-      
+    settings = {
       log.level = "info";
 
       inbounds = [
-      	{
-	  type = "tun";
-	  tag = "tun-in";
-	  interface_name = "tun0";
-	  inet4_address = "172.19.0.1/30";
-	  auto_route = true;
-	  strict_route = true;
-	  stack = "system";
-	}
+        {
+          type = "tun";
+          tag = "tun-in";
+          interface_name = "tun0";
+          inet4_address = "172.19.0.1/30";
+          auto_route = true;
+          strict_route = true;
+          stack = "system";
+        }
       ];
 
       outbounds = [
-      	{ type = "direct"; tag = "direct"; }
-	{
-	  type = "vless";
-	  tag = "proxy";
-	  server = ip;
-	  server_port = 8443;
-	  uuid = uuid; 
-	  transport.type = "tcp";
+        { type = "direct"; tag = "direct"; }
+        {
+          type = "vless";
+          tag = "proxy";
+          server = {
+            _secret = config.sops.secrets.ip.path;
+          };
+          server_port = 8443;
+          uuid = {
+            _secret = config.sops.secrets.uuid.path;
+          };
+          transport.type = "tcp";
 
-	  tls = {
-	    enabled = true;
-	    server_name = "googletagmanager.com";
-	    reality = {
-	      short_id = sid;
-	      public_key = "0hKXovW8oVrg01lCNbKm0eBp20L_fY6aW2fvdphif3c";
-	    };
-	  };
-	}
+          tls = {
+            enabled = true;
+            server_name = "googletagmanager.com";
+            reality = {
+              short_id = {
+                _secret = config.sops.secrets.sid.path;
+              };
+              public_key = "0hKXovW8oVrg01lCNbKm0eBp20L_fY6aW2fvdphif3c";
+            };
+          };
+        }
       ];
 
       route = {
-	geoip.path = "${pkgs.sing-geoip}/share/sing-box/geoip.db";
-	geosite.path = "${pkgs.sing-geosite}/share/sing-box/geosite.db";
+        geoip.path = "${pkgs.sing-geoip}/share/sing-box/geoip.db";
+        geosite.path = "${pkgs.sing-geosite}/share/sing-box/geosite.db";
 
-	rules = [
-	  { geoip = [ "ru" ]; geosite = [ "ru" ]; outbound = "direct"; }
-	  { ip_cidr = [ "0.0.0.0/0" "::/0" ]; outbound = "proxy"; }
-	];
+        rules = [
+          { geoip = [ "ru" ]; geosite = [ "ru" ]; outbound = "direct"; }
+          { ip_cidr = [ "0.0.0.0/0" "::/0" ]; outbound = "proxy"; }
+        ];
       };
     };
   };
