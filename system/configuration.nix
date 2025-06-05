03a0dd3b7142bb3e7ebe668475677c365e53ea5f
wiyba <account@wiyba.org>
2025-06-05 11:07:25 +0300

    move singbox

--- system/configuration.nix
+++ system/configuration.nix
@@ -8,7 +8,7 @@ let
   myfonts = pkgs.callPackage fonts/default.nix { inherit pkgs; };
 in
 {
-  imports = [ ../home/programs/sing-box (import ../secrets) ];
+  imports = [ ./misc/singbox (import ../secrets) ];
 
   networking = {
     extraHosts = "";
