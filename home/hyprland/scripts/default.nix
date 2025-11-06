c8e587a3892379f61a2e39821c7ca17cfe336e2e
wiyba <account@wiyba.org>
2025-11-06 10:58:46 +0300

    fix

--- home/hyprland/scripts/default.nix
+++ home/hyprland/scripts/default.nix
@@ -3,13 +3,11 @@ let
     let
       csw = pkgs.callPackage ./close-special-workspace.nix { };
       kls = pkgs.callPackage ./keyboard-layout-switch.nix { };
-      szp = pkgs.callPackage ./show-zombie-parents.nix { };
     in
     {
       home.packages = [
         csw
         kls
-        szp
       ];
     };
 in
