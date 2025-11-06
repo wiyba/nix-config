a873bcc1c56f114bb854a7513818f5e3aa722773
wiyba <account@wiyba.org>
2025-11-06 10:58:46 +0300

    fix

--- home/base/scripts/default.nix
+++ home/base/scripts/default.nix
@@ -1,14 +1,10 @@
 let
   scripts = { config, lib, pkgs, ... }:
     let
-      csw = pkgs.callPackage ./close-special-workspace.nix { };
-      kls = pkgs.callPackage ./keyboard-layout-switch.nix { };
       szp = pkgs.callPackage ./show-zombie-parents.nix { };
     in
     {
       home.packages = [
-        csw
-        kls
         szp
       ];
     };
