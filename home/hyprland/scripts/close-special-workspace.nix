840c4b36badec6ec594b171f858c291afe02208b
wiyba <account@wiyba.org>
2025-11-06 11:08:08 +0300

    fix

--- home/hyprland/scripts/close-special-workspace.nix
+++ home/hyprland/scripts/close-special-workspace.nix
@@ -1,10 +1,9 @@
 { writeShellScriptBin, hyprland, jq, ... }:
 let
   hyprctl = "${hyprland}/bin/hyprctl";
-  jq = "${jq}/bin/jq";
 in
 writeShellScriptBin "csw" ''
-  active=$(${hyprctl} -j monitors | ${jq} --raw-output '.[] | select(.focused==true).specialWorkspace.name | split(":") | if length > 1 then .[1] else "" end')
+  active=$(${hyprctl} -j monitors | ${jq}/bin/jq --raw-output '.[] | select(.focused==true).specialWorkspace.name | split(":") | if length > 1 then .[1] else "" end')
   if [[ ''${#active} -gt 0 ]]; then
     ${hyprctl} dispatch togglespecialworkspace "$active"
   fi
