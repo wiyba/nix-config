03c76659c94ff59a2b41f937c4cff90a53bd4af5
Dmitry Shmakov <81859776+wiyba@users.noreply.github.com>
2025-11-07 21:38:11 +0300

    Update default.nix

--- home/base/programs/git/default.nix
+++ home/base/programs/git/default.nix
@@ -1,38 +1,40 @@
 { lib, pkgs, ... }:
-
 {
-  home.packages = with pkgs.gitAndTools; [
-    diff-so-fancy # git diff with colors
-    git-crypt # git files encryption
-    hub # github command-line client
-    tig # diff and commit view
+  home.packages = with pkgs; [
+    diff-so-fancy
+    git-crypt
+    hub
+    tig
   ];
   
   programs.git = {
     enable = true;
-    userName = "wiyba";
-    userEmail = "account@wiyba.org";
-    extraConfig.core.editor = "nvim";
-
-    aliases = {
-      amend = "commit --amend -m";
-      br = "branch";
-      co = "checkout";
-      cob = "checkout -b";
-      st = "status";
-      ls = "log --pretty=format:\"%C(yellow)%h%Cred%d\\\\ %Creset%s%Cblue\\\\ [%cn]\" --decorate";
-      ll = "log --pretty=format:\"%C(yellow)%h%Cred%d\\\\ %Creset%s%Cblue\\\\ [%cn]\" --decorate --numstat";
-      cm = "commit -m";
-      ca = "commit -am";
-      dc = "diff --cached";
-      rmain = "rebase main";
-      rc = "rebase --continue";
-    };
-
-    extraConfig = {
+    
+    settings = {
+      user = {
+        name = "wiyba";
+        email = "account@wiyba.org";
+      };
+      
+      core.editor = "nvim";
       init.defaultBranch = "main";
+      
+      alias = {
+        amend = "commit --amend -m";
+        br = "branch";
+        co = "checkout";
+        cob = "checkout -b";
+        st = "status";
+        ls = "log --pretty=format:\"%C(yellow)%h%Cred%d\\\\ %Creset%s%Cblue\\\\ [%cn]\" --decorate";
+        ll = "log --pretty=format:\"%C(yellow)%h%Cred%d\\\\ %Creset%s%Cblue\\\\ [%cn]\" --decorate --numstat";
+        cm = "commit -m";
+        ca = "commit -am";
+        dc = "diff --cached";
+        rmain = "rebase main";
+        rc = "rebase --continue";
+      };
     };
-
+    
     ignores = [
       ".DS_Store"
     ];
