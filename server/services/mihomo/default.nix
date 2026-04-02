be84ac8d7515c8768e8bb456435556cb66709bdc
wiyba <account@wiyba.org>
2026-04-02 21:49:20 +0300

    ???

--- server/services/mihomo/default.nix
+++ server/services/mihomo/default.nix
@@ -116,9 +116,8 @@
         - GEOSITE,godaddy,LONDON
         - GEOSITE,wix,LONDON
         - GEOSITE,patreon,LONDON
-        - DOMAIN-SUFFIX,ntc.party,LONDON
         - GEOIP,PRIVATE,DIRECT
-        - IP-CIDR6,::/0,LONDON,no-resolve
+        - IP-CIDR6,::/0,LONDON
         - MATCH,DIRECT
     '';
     path = "/etc/mihomo/config.yaml";
